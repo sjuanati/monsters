07ca49b3a37f825a7bd3468a98c1a76cb372fedf
Sergi Juanati <wint3rmute@gmail.com>
2021-02-27 17:15:52 +0100

    Only one ERC721 contract

--- contracts/Monsters.sol
+++ contracts/Monsters.sol
@@ -1,52 +1,93 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-import "./NFT.sol";
 import "./access/Ownable.sol";
 import "./token/ERC721/ERC721.sol";
 
 /**
- * @title   Monsters
+ * @title   NFT
  * @notice  xxxxx
  */
-contract Monsters is Ownable {
-    NFT public nft;
+contract Monsters is ERC721, Ownable {
+    using Strings for uint256;
 
-    constructor(string memory _name, string memory _symbol) {
-        nft = new NFT(_name, _symbol);
+    // Optional mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
+
+    // Base URI
+    string private _baseURIextended;
+
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {}
+
+    function setBaseURI(string memory baseURI_) external onlyOwner() {
+        _baseURIextended = baseURI_;
+    }
+
+    // Adapted for OpenSea
+    function baseTokenURI() external view returns (string memory) {
+        return _baseURIextended;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+        // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
+        return string(abi.encodePacked(base, tokenId.toString()));
     }
 
     function mint(
         address _to,
         uint256 _tokenId,
-        string memory _tokenURI
+        string memory tokenURI_
     ) external onlyOwner() {
-        nft.mint(_to, _tokenId, _tokenURI);
+        _mint(_to, _tokenId);
+        _setTokenURI(_tokenId, tokenURI_);
     }
 
-    // For testing
-    function burn(uint256 _tokenId) external onlyOwner() {
-        nft.burn(_tokenId);
+    function burn(uint256 tokenId) external onlyOwner() {
+        _burn(tokenId);
     }
 
-    function setBaseURI(string memory _baseURI) external onlyOwner() {
-        nft.setBaseURI(_baseURI);
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+        internal
+        virtual
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI set of nonexistent token"
+        );
+        _tokenURIs[tokenId] = _tokenURI;
     }
 
-    function baseTokenURI() external view returns (string memory) {
-        return nft.baseTokenURI();
-    }
-
-    function tokenURI(uint256 _tokenId) external view returns (string memory) {
-        return nft.tokenURI(_tokenId);
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURIextended;
     }
 }
 
-// Contract address Rinkeby: 0xe7f9fD99B63fBB53148b4963d24c5bF251BE430d
+// base URL :           https://www.bitacoras.io/monsters/
+// contract address:    0xEC2Dd1E167B36F6cd0a8eC1E5bFB2ADF291D9EED
+// view                 https://testnets.opensea.io/assets/0xEC2Dd1E167B36F6cd0a8eC1E5bFB2ADF291D9EED/1
+// test                 https://testnets-api.opensea.io/asset/0xEC2Dd1E167B36F6cd0a8eC1E5bFB2ADF291D9EED/1
 
-/*
-// Cranc
-https://storage.googleapis.com/opensea-prod.appspot.com/creature/50.png
-https://storage.googleapis.com/opensea-prod.appspot.com/creature/51.png
-
-*/
