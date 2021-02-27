f081014a233f64726203787e8bac5c7f19eacfef
Sergi Juanati <wint3rmute@gmail.com>
2021-02-27 12:25:42 +0100

    Added burn() for testing

--- contracts/Monsters.sol
+++ contracts/Monsters.sol
@@ -10,32 +10,43 @@ import "./token/ERC721/ERC721.sol";
  * @notice  xxxxx
  */
 contract Monsters is Ownable {
-    
     NFT public nft;
-    
+
     constructor(string memory _name, string memory _symbol) {
         nft = new NFT(_name, _symbol);
     }
-    /*
-    function create(string memory _name, string memory _symbol) external onlyOwner() {
-        nft = new NFT(_name, _symbol);
-    }
-    */
-    function mint(address _to, uint256 _tokenId, string memory _tokenURI) external onlyOwner() {
+
+    function mint(
+        address _to,
+        uint256 _tokenId,
+        string memory _tokenURI
+    ) external onlyOwner() {
         nft.mint(_to, _tokenId, _tokenURI);
     }
-    
+
+    // For testing
+    function burn(uint256 _tokenId) external onlyOwner() {
+        nft.burn(_tokenId);
+    }
+
     function setBaseURI(string memory _baseURI) external onlyOwner() {
         nft.setBaseURI(_baseURI);
     }
-    
-    function tokenURI(uint256 _tokenId) external view returns (string memory) {
-        return nft.tokenURI(_tokenId);
-    }
-    
+
     function baseTokenURI() external view returns (string memory) {
         return nft.baseTokenURI();
     }
+
+    function tokenURI(uint256 _tokenId) external view returns (string memory) {
+        return nft.tokenURI(_tokenId);
+    }
 }
 
-// Contract address Rinkeby: 0x88719e766Be28ede552CBDC15F7C137eD3833524
+// Contract address Rinkeby: 0xe7f9fD99B63fBB53148b4963d24c5bF251BE430d
+
+/*
+// Cranc
+https://storage.googleapis.com/opensea-prod.appspot.com/creature/50.png
+https://storage.googleapis.com/opensea-prod.appspot.com/creature/51.png
+
+*/
--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -9,44 +9,57 @@ import "./token/ERC721/ERC721.sol";
  * @notice  xxxxx
  */
 contract NFT is ERC721, Ownable {
-    
     using Strings for uint256;
-    
+
     // Optional mapping for token URIs
-    mapping (uint256 => string) private _tokenURIs;
+    mapping(uint256 => string) private _tokenURIs;
 
     // Base URI
     string private _baseURIextended;
 
-
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
     {}
-    
+
     function setBaseURI(string memory baseURI_) external onlyOwner() {
         _baseURIextended = baseURI_;
     }
-    
-    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
-        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+        internal
+        virtual
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI set of nonexistent token"
+        );
         _tokenURIs[tokenId] = _tokenURI;
     }
-    
+
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseURIextended;
     }
-    
+
     // Adapted for OpenSea
     function baseTokenURI() external view returns (string memory) {
         return _baseURIextended;
     }
-    
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
 
         string memory _tokenURI = _tokenURIs[tokenId];
         string memory base = _baseURI();
-        
+
         // If there is no base URI, return the token URI.
         if (bytes(base).length == 0) {
             return _tokenURI;
@@ -58,7 +71,7 @@ contract NFT is ERC721, Ownable {
         // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
         return string(abi.encodePacked(base, tokenId.toString()));
     }
-    
+
     function mint(
         address _to,
         uint256 _tokenId,
@@ -66,5 +79,9 @@ contract NFT is ERC721, Ownable {
     ) external onlyOwner() {
         _mint(_to, _tokenId);
         _setTokenURI(_tokenId, tokenURI_);
+    }
+
+    function burn(uint256 tokenId) external onlyOwner() {
+        _burn(tokenId);
     }
 }
